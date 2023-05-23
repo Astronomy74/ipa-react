7b98841459106d9058e0b40eff7617509792741f
AmjedNazzal <amjednazzal@outlook.com>
2023-05-24 00:30:44 +0300

    added download

--- src/components/conversation.js
+++ src/components/conversation.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useLocation } from 'react-router-dom';
 import NavBar from "./navBar";
 import UploadButton from "./uploadButton";
+import download from 'downloadjs';
 import { ref, listAll, getDownloadURL, uploadBytesResumable, deleteObject } from "firebase/storage";
 import { storage } from "./fireStorage";
 import { Link, useNavigate } from 'react-router-dom';
@@ -28,9 +29,12 @@ function Conversation(props){
     const timestamp = new Date(); // Get current timestamp
     const formattedDate = timestamp.toISOString(); // Format timestamp as a string
 
+    
 
-    const submitMessage = async (attachmentLink, attachmentName) => {
+    ///////////// Handle Message Submit /////////////
 
+    const submitMessage = async (folderName ,attachmentLink, attachmentName) => {
+     
       const messageData = { // this is the structure I'm using, a map type field in the database
             sender: props.userInfo.login.email,
             receiver: 'coordinator@example.com', // replace later with real coordinator email
@@ -46,21 +50,31 @@ function Conversation(props){
       const Participants = [props.userInfo.login.email, 'coordinator@example.com'];
       
       try {
-        const conversationCollectionRef = collection(db, 'conversations');
-        // // check if a document with the same subject exists, cuz it'll be updated if true
-        // const querySnapshot = await getDocs(query(conversationCollectionRef, where('subject', '==', Subject)));
-        
-        if (lastItem === 'new') {
+        const conversationCollectionRef = collection(db, 'conversations');       
 
-          // if no existing conversation with the same subject, create a new document
-          const newConversationRef = await addDoc(conversationCollectionRef, {
-            subject: Subject,
-            [formattedDate]: messageData,
-            participants: Participants,
-          });
-          console.log('new conversation created with id:', newConversationRef.id);
-        } 
+        // If it's a new message
+        if(lastItem === 'new'){
+
+          // if it's a new message that has an attachment we use foldername to match ids
+          if(folderName){
+            const conversationDocRef = doc(conversationCollectionRef, folderName);
+            await setDoc(conversationDocRef, {
+              subject: Subject,
+              [formattedDate]: messageData,
+              participants: Participants,
+            });
+          }
+          else{
+            // if it's a new message that doesn't have an attachment we create a new id
+            await addDoc(conversationCollectionRef, {
+              subject: Subject,
+              [formattedDate]: messageData,
+              participants: Participants,
+            });
+          }
+        }
         else {
+          // if it's not a new message 
           const conversationDocRef = doc(conversationCollectionRef, lastItem);
           const querySnapshot = await getDoc(query(conversationDocRef));
           const conversationData = querySnapshot.data();
@@ -85,23 +99,38 @@ function Conversation(props){
     }
     
     // Handle Attachment
+
+    let folderName = '';
+    let uploadFolderRef = '';
     
     if (Attachment) {
-      const storageRef = ref(storage, `conversations/`);
-      const folderName = lastItem;
-  
+
       try {
-        const res = await listAll(storageRef);
-        const existingFolder = res.prefixes.find((prefix) => prefix.name === folderName);
-        let uploadFolderRef = '';
-  
-        if (existingFolder) {
-          // Folder with the same name already exists, save inside the existing folder
-          uploadFolderRef = ref(storage, `${existingFolder.fullPath}/${formattedDate}`);
+        const extention = Attachment.name.split('.').pop().toLowerCase();
+
+        if(lastItem === 'new'){
+          // Generate an automatic ID for the folder
+          const newConversationRef = await addDoc(collection(db, 'conversations'), {});
+          // Get the newly generated document ID
+          const newConversationId = newConversationRef.id;
+          // Construct the storage reference using the new document ID and formattedDate
+          uploadFolderRef = ref(storage, `conversations/${newConversationId}/${formattedDate}.${extention}`);
+          folderName = newConversationId;
         } else {
-          // Folder with the same name doesn't exist, create a new folder
-          uploadFolderRef = ref(storage, `conversations/${folderName}/${formattedDate}`);
+          const storageRef = ref(storage, `conversations/`);
+          const res = await listAll(storageRef);
+          const existingFolder = res.prefixes.find((prefix) => prefix.name === lastItem);
+
+          if (existingFolder) {
+            // Folder with the same name already exists, save inside the existing folder
+            uploadFolderRef = ref(storage, `${existingFolder.fullPath}/${formattedDate}.${extention}`);
+          } else {
+            // Folder with the same name doesn't exist, create a new folder
+            uploadFolderRef = ref(storage, `conversations/${lastItem}/${formattedDate}.${extention}`);
+          }
+
         }
+    
         const uploadTask = uploadBytesResumable(uploadFolderRef, Attachment);
         uploadTask.on(
           "state_changed",
@@ -113,12 +142,13 @@ function Conversation(props){
           () => {
             UploadedAttachment("");
           // Get the download URL of the uploaded file
-            getDownloadURL(uploadFolderRef).then((url) => {
+            const newRef = uploadFolderRef._location.path;
+            const uploadedFileRef = ref(storage, newRef);
+            getDownloadURL(uploadedFileRef).then((url) => {
               let downloadURL = url;
               let attachmentName = Attachment.name;
-
             // Proceed with message submission or any other action here
-            submitMessage(downloadURL, attachmentName);
+            submitMessage(folderName, downloadURL, attachmentName);
           });
           }
           );
@@ -126,15 +156,17 @@ function Conversation(props){
           // Handle error
           console.error("Error handling attachment:", error);
           }
-          } else {
-          // No attachment, proceed with message submission or any other action here
-          submitMessage("");
-          }
+    } 
+    else {
+        // No attachment
+        submitMessage("");
+    }
 
     }
-  
-    // ...
-  
+
+  ////////// Handle Message Submit /////////
+
+
 
   function GetAttachment(file) {
     UploadedAttachment(file);
@@ -142,64 +174,98 @@ function Conversation(props){
 
   const [conversation, setConversation] = useState([]);
 
-  useEffect(() => {
-  const db = getFirestore();
-  const conversationsCollection = collection(db, "conversations");
-  const conversationDocRef = doc(conversationsCollection, lastItem);
-  getDoc(conversationDocRef)
-  .then((querySnapshot) => {
-      const conversationsCollect = querySnapshot.data();
-      const filteredConv = [];
-      Object.keys(conversationsCollect).forEach((key) => {
-        const value = conversationsCollect[key];
-        if(key !== 'participants' && key !== 'subject'){
-          filteredConv.push(value);
-        }
-      });
-      UpdateDetector(null);
-      setConversation(filteredConv)
-      
-
-  })
-  .catch((error) => {
-      console.log("Error getting documents: ", error); // if fetching documents failed
-  });
-  }, [UpdateDetect]);
-
-
-  const renderConversation = conversation
-  .sort((a, b) => a.timestamp.toDate() - b.timestamp.toDate()) // Sort by timestamp
-  .map((entry) => {
-    const msgDate = entry.timestamp.toDate();
-    const formattedDate = msgDate.toLocaleDateString(); // Format the date
-    const formattedTime = msgDate.toLocaleTimeString(); // Format the time
-
   
-    return(
-        <div className="box" key={msgDate}>
-            <div className="box-title">
-                {/* <Link to={`/student-dashboard/stats/${box.title.replace(/\s/g, "-")}`}>
-                    <span 
-                    className="internshipLink"
-                    data-json="data/internship.json"
-                    >
-                    {box.title}
-                    </span>
-                </Link> */}
-                <span>{entry.sender}</span>
-                <span>{formattedDate} {formattedTime}</span>
-                <div>
-                  <div>
-                  {entry.message}
-                  </div>
-                  <div>
-                  <a href={entry.attachmentLink}><span>{entry.attachmentName}</span></a>
-                  </div>
+    useEffect(() => {
+      if(lastItem !== 'new'){
+      const db = getFirestore();
+      const conversationsCollection = collection(db, "conversations");
+      const conversationDocRef = doc(conversationsCollection, lastItem);
+      getDoc(conversationDocRef)
+      .then((querySnapshot) => {
+          const conversationsCollect = querySnapshot.data();
+          const filteredConv = [];
+          Object.keys(conversationsCollect).forEach((key) => {
+            const value = conversationsCollect[key];
+            if(key !== 'participants' && key !== 'subject'){
+              filteredConv.push(value);
+            }
+          });
+          UpdateDetector(null);
+          setConversation(filteredConv)
+          
+    
+      })
+      .catch((error) => {
+          console.log("Error getting documents: ", error); // if fetching documents failed
+      });
+      }
+      }, [UpdateDetect]);
+  
+  
+  
+      const renderConversations = conversation.sort((a, b) => a.timestamp.toDate() - b.timestamp.toDate()) // Sort by timestamp
+      .map((entry) => {
+        const msgDate = entry.timestamp.toDate();
+        const formattedDate = msgDate.toLocaleDateString(); // Format the date
+        const formattedTime = msgDate.toLocaleTimeString(); // Format the time
+        
+
+        function downloadFile(event) {
+          event.preventDefault();
+        
+          let anchor = event.target;
+          
+          let url = anchor.getAttribute("href");
+        
+          const xhr = new XMLHttpRequest();
+          xhr.responseType = "blob";
+          xhr.onload = function(event){
+            const blob = xhr.response;
+            const blobUrl = window.URL.createObjectURL(blob);
+        
+            const a = document.createElement('a');
+            a.href = blobUrl;
+            a.download = anchor.getAttribute("download");
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        
+            window.URL.revokeObjectURL(blobUrl);
+          };
+          xhr.open("GET", url);
+          xhr.send();
+        }
+        
+        
+
+        return(
+            <div className="box" key={msgDate}>
+                <div className="box-title">
+                    {/* <Link to={`/student-dashboard/stats/${box.title.replace(/\s/g, "-")}`}>
+                        <span 
+                        className="internshipLink"
+                        data-json="data/internship.json"
+                        >
+                        {box.title}
+                        </span>
+                    </Link> */}
+                    <span>{entry.sender}</span>
+                    <span>{formattedDate} {formattedTime}</span>
+                    <div>
+                      <div>
+                      {entry.message}
+                      </div>
+                      <div>
+                      <a href={entry.attachmentLink} download={entry.attachmentName} onClick={downloadFile}>{entry.attachmentName}</a>
+                      </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-});
+        );
+        });
+   
+    
+  
 
 
 
@@ -211,7 +277,7 @@ function Conversation(props){
                 <div className="col-md-12">
                     <div className="login">
                         <h1>Message</h1>
-                        {renderConversation}
+                        {renderConversations}
                         <form className="login-form needs-validation" noValidate onSubmit={handleSubmit}>
                           {lastItem === 'new' ? (
                               <div className="mb-3">
@@ -226,7 +292,7 @@ function Conversation(props){
                                 value={Subject}
                                 onChange={(e) => setSubject(e.target.value)}
                               />
-                              Message
+                              Subject
                               </div>
                             ) : (
                               <div></div>
